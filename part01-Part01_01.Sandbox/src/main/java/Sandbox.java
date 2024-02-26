f52f1be1a614812fd45e2938678bea381ab1e745
kovalen1618 <kovalen1.618@gmail.com>
2024-02-26 17:45:05 -0600

    Completes 1-4 using System.out.println()

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,5 +3,9 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
+        System.out.println("Welcome to programming!");
+        
+        // Typing 'sout' and tab types out System.out.println();
+        System.out.println("");
     }
 }
